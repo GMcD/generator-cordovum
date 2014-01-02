7f99841edf06324434204bf68dc7b10e43208720
GMcD <gary.macdonald@projectscapa.com>
2014-01-02 14:00:58 +0000

    Added initial ImageMagick image conversion for iOS assets.

---
+++ app/templates/_icons.js
@@ -0,0 +1,45 @@
+var fs = require('fs');
+var im = require('imagemagick');
+var spawn = require('child_process').spawn;
+
+var imgFolder = __dirname + '/app/img/';
+var iosImgFolder = imgFolder + 'ios';
+var iosIconFolder = iosImgFolder + '/icons';
+var iosSplashFolder = iosImgFolder + '/splash';
+
+var iconPng = imgFolder + 'imp.png';
+var splashPng = imgFolder + 'impFace.png';
+
+/* fs.mkdirSync(iosImgFolder); */
+
+/* iOS Icon Images */
+im.resize({ srcPath: iconPng, dstPath: iosIconFolder + '/icon-40.png', width: 40 });
+im.resize({ srcPath: iconPng, dstPath: iosIconFolder + '/icon-40@2x.png', width: 80 });
+im.resize({ srcPath: iconPng, dstPath: iosIconFolder + '/icon-50.png', width: 50 });
+im.resize({ srcPath: iconPng, dstPath: iosIconFolder + '/icon-50@2x.png', width: 100 });
+im.resize({ srcPath: iconPng, dstPath: iosIconFolder + '/icon-60.png', width: 60 });
+im.resize({ srcPath: iconPng, dstPath: iosIconFolder + '/icon-60@2x.png', width: 120 });
+im.resize({ srcPath: iconPng, dstPath: iosIconFolder + '/icon-76.png', width: 76 });
+im.resize({ srcPath: iconPng, dstPath: iosIconFolder + '/icon-76@2x.png', width: 152 });
+im.resize({ srcPath: iconPng, dstPath: iosIconFolder + '/icon-small.png', width: 29 });
+im.resize({ srcPath: iconPng, dstPath: iosIconFolder + '/icon-small@2x.png', width: 58 });
+im.resize({ srcPath: iconPng, dstPath: iosIconFolder + '/icon-72.png', width: 72 });
+im.resize({ srcPath: iconPng, dstPath: iosIconFolder + '/icon-72@2x.png', width: 144 });
+im.resize({ srcPath: iconPng, dstPath: iosIconFolder + '/icon.png', width: 57 });
+im.resize({ srcPath: iconPng, dstPath: iosIconFolder + '/icon@2x.png', width: 114 });
+
+/* fs.copy iosIconFolder impromptu/platforms/ios/impromptu/Resources/icons */
+
+// im.resize({ srcData : splashPng, strip : false, width : 173, height : "173^", customArgs: [ "-gravity", "center", "-extent", "173x173"] };
+	
+// im.resize({ srcData : splashPng, dstPath: iosSplashFolder + '/Default-568h@2x~iphone.png', width: 640, customArgs: [ "-gravity", "center", "-extent", "640x1136"] });
+
+spawn('convert', [splashPng, '-gravity', 'center', '-extent',  '640x1136', iosSplashFolder + '/Default-568h@2x~iphone.png']);
+spawn('convert', [splashPng, '-gravity', 'center', '-extent', '2048x1496', iosSplashFolder + '/Default-Landscape@2x~ipad.png']);
+spawn('convert', [splashPng, '-gravity', 'center', '-extent',  '1024x748', iosSplashFolder + '/Default-Landscape~ipad.png']);
+spawn('convert', [splashPng, '-gravity', 'center', '-extent', '1536x2008', iosSplashFolder + '/Default-Portrait@2x~ipad.png']);
+spawn('convert', [splashPng, '-gravity', 'center', '-extent',  '768x1004', iosSplashFolder + '/Default-Portrait~ipad.png']);
+spawn('convert', [splashPng, '-gravity', 'center', '-extent',   '640x960', iosSplashFolder + '/Default@2x~iphone.png']);
+spawn('convert', [splashPng, '-gravity', 'center', '-extent',   '320x480', iosSplashFolder + '/Default~iphone.png']);
+
+
