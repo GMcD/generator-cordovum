20334dc60b5329ae43b5ea3a73d356d28abd5a94
GMcD <gary.macdonald@projectscapa.com>
2014-08-19 12:52:48 +0100

    Monolithic app copy...

--- app/index.js
+++ app/index.js
@@ -82,44 +82,41 @@ CordovumGenerator.prototype.app = function app() {
   this.mkdir('app/img');
 
   this.mkdir('app/js');
-  this.copy('js/_app.js', 'app/js/app.js')
-  this.copy('js/_router.js', 'app/js/router.js')
-  this.copy('js/_home.js', 'app/js/home.js')
+  this.copy('js/_app.js', 'app/js/app.js');
+  this.copy('js/_router.js', 'app/js/router.js');
+  this.copy('js/_home.js', 'app/js/home.js');
 
   this.mkdir('app/js/libs');
-  this.copy('js/libs/_jquery.jscrollpane.js', 'app/js/libs/jquery.jscrollpane.js')
-  this.copy('js/libs/_jquery.touchy.js', 'app/js/libs/jquery.touchy.js')
-  this.copy('js/libs/_jquery.ui.touch-punch.js', 'app/js/libs/jquery.ui.touch-punch.js')
-  this.copy('js/libs/_jsonform.js', 'app/js/libs/jsonform.js')
-  this.copy('js/libs/_jsv.js', 'app/js/libs/jsv.js')
-  this.copy('js/libs/_utils.js', 'app/js/libs/utils.js')
+  this.copy('js/libs/_jquery.jscrollpane.js', 'app/js/libs/jquery.jscrollpane.js');
+  this.copy('js/libs/_jquery.touchy.js', 'app/js/libs/jquery.touchy.js');
+  this.copy('js/libs/_jquery.ui.touch-punch.js', 'app/js/libs/jquery.ui.touch-punch.js');
+  this.copy('js/libs/_jsonform.js', 'app/js/libs/jsonform.js');
+  this.copy('js/libs/_jsv.js', 'app/js/libs/jsv.js');
+  this.copy('js/libs/_utils.js', 'app/js/libs/utils.js');
   
   this.mkdir('app/scss');
-  this.copy('scss/_app.scss', 'app/scss/app.scss')
-  this.copy('scss/__home.scss', 'app/scss/_home.scss')
-  this.copy('scss/__colours.scss', 'app/scss/_colours.scss')
-  this.copy('scss/__variables.custom.scss', 'app/scss/_variables.custom.scss')
-  this.copy('scss/__html_inputs.scss', 'app/scss/_html_inputs.scss')
-  this.copy('scss/__ui_elements.scss', 'app/scss/_ui_elements.scss')
-  this.copy('scss/__form_buttons.scss', 'app/scss/_form_buttons.scss')
-  this.copy('scss/__table_columns.scss', 'app/scss/_table_columns.scss')
-  this.copy('scss/__page_layouts.scss', 'app/scss/_page_layouts.scss')
-  this.copy('scss/__media_queries.scss', 'app/scss/_media_queries.scss')
+  this.copy('scss/_app.scss', 'app/scss/app.scss');
+  this.copy('scss/__home.scss', 'app/scss/_home.scss');
+  this.copy('scss/__colours.scss', 'app/scss/_colours.scss');
+  this.copy('scss/__variables.custom.scss', 'app/scss/_variables.custom.scss');
+  this.copy('scss/__html_inputs.scss', 'app/scss/_html_inputs.scss');
+  this.copy('scss/__ui_elements.scss', 'app/scss/_ui_elements.scss');
+  this.copy('scss/__form_buttons.scss', 'app/scss/_form_buttons.scss');
+  this.copy('scss/__table_columns.scss', 'app/scss/_table_columns.scss');
+  this.copy('scss/__page_layouts.scss', 'app/scss/_page_layouts.scss');
+  this.copy('scss/__media_queries.scss', 'app/scss/_media_queries.scss');
   
   this.mkdir('app/tpl');
-  this.copy('tpl/_homeTemplate.html', 'app/tpl/homeTemplate.html')
+  this.copy('tpl/_homeTemplate.html', 'app/tpl/homeTemplate.html');
   
   this.mkdir('app/tests');
-  this.copy('tests/_home.js', 'app/tests/home.js')
+  this.copy('tests/_home.js', 'app/tests/home.js');
   
   this.copy('_app.html', 'app/app.html');
   this.copy('_jasmine.html', 'app/jasmine.html');
   this.copy('_require.app.js', 'app/require.app.js');
   this.copy('_require.jasmine.js', 'app/require.jasmine.js');
 
-};
-
-CordovumGenerator.prototype.app = function app() {
   this.mkdir('app/scss/spinner');
   this.copy('scss/spinner/_indicator.base.scss', 'app/scss/spinner/_indicator.base.scss');  
   this.copy('scss/spinner/_indicator.fblocks.scss', 'app/scss/spinner/_indicator.fblocks.scss');
